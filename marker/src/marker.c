82d6640cd958a2eca9b17b7d757238f809961f74
NotAShelf <raf@notashelf.dev>
2025-08-27 09:21:37 +0300

    marker: reformat
    Signed-off-by: NotAShelf <raf@notashelf.dev>
    Change-Id: I6a6a69644e7f90179b80a188f4acc3dcba07bf1c

--- marker/src/marker.c
+++ marker/src/marker.c
@@ -12,36 +12,29 @@
 // - Inline code: `code`
 // - Links: [text](url)
 // - Escape characters: \*, \_, \#, etc.
-void inline_parse(const char* in, char* out, size_t out_size)
-{
+void inline_parse(const char* in, char* out, size_t out_size) {
   size_t i = 0, j = 0;
-  while (in[i] && j < out_size - 1)
-  {
-    if (in[i] == '\\')
-    {
-      if (in[i + 1])
-      {  // add the escaped character as is
+  while (in[i] && j < out_size - 1) {
+    if (in[i] == '\\') {
+      if (in[i + 1]) {  // add the escaped character as is
         out[j++] = in[i + 1];
         i += 2;
         continue;
       }
     }
 
-    if (in[i] == '*' && in[i + 1] == '*')
-    {
+    if (in[i] == '*' && in[i + 1] == '*') {
       const char* openTag = "<b>";
       size_t      k       = 0;
       while (openTag[k] && j < out_size - 1)
         out[j++] = openTag[k++];
       i += 2;
-      while (!(in[i] == '*' && in[i + 1] == '*') && in[i] != '\0')
-      {
+      while (!(in[i] == '*' && in[i + 1] == '*') && in[i] != '\0') {
         out[j++] = in[i++];
         if (j >= out_size - 1)
           break;
       }
-      if (in[i] == '*' && in[i + 1] == '*')
-      {
+      if (in[i] == '*' && in[i + 1] == '*') {
         const char* closeTag = "</b>";
         k                    = 0;
         while (closeTag[k] && j < out_size - 1)
@@ -51,21 +44,18 @@ void inline_parse(const char* in, char* out, size_t out_size)
       continue;
     }
 
-    if (in[i] == '*')
-    {
+    if (in[i] == '*') {
       const char* openTag = "<i>";
       size_t      k       = 0;
       while (openTag[k] && j < out_size - 1)
         out[j++] = openTag[k++];
       i++;
-      while (in[i] != '*' && in[i] != '\0')
-      {
+      while (in[i] != '*' && in[i] != '\0') {
         out[j++] = in[i++];
         if (j >= out_size - 1)
           break;
       }
-      if (in[i] == '*')
-      {
+      if (in[i] == '*') {
         const char* closeTag = "</i>";
         k                    = 0;
         while (closeTag[k] && j < out_size - 1)
@@ -75,23 +65,20 @@ void inline_parse(const char* in, char* out, size_t out_size)
       continue;
     }
 
-    if (in[i] == '`')
-    {
+    if (in[i] == '`') {
       const char* openTag = "<code>";
       size_t      k       = 0;
       while (openTag[k] && j < out_size - 1)
         out[j++] = openTag[k++];
       i++;
 
-      while (in[i] != '`' && in[i] != '\0')
-      {
+      while (in[i] != '`' && in[i] != '\0') {
         out[j++] = in[i++];
         if (j >= out_size - 1)
           break;
       }
 
-      if (in[i] == '`')
-      {
+      if (in[i] == '`') {
         const char* closeTag = "</code>";
         k                    = 0;
         while (closeTag[k] && j < out_size - 1)
@@ -102,28 +89,23 @@ void inline_parse(const char* in, char* out, size_t out_size)
       continue;
     }
 
-    if (in[i] == '[')
-    {
+    if (in[i] == '[') {
       size_t link_text_start = i + 1;
       size_t text_len        = 0;
       while (in[link_text_start + text_len] && in[link_text_start + text_len] != ']')
         text_len++;
-      if (in[link_text_start + text_len] == ']')
-      {
-        if (in[link_text_start + text_len + 1] == '(')
-        {
+      if (in[link_text_start + text_len] == ']') {
+        if (in[link_text_start + text_len + 1] == '(') {
           size_t url_start = link_text_start + text_len + 2;
           size_t url_len   = 0;
           while (in[url_start + url_len] && in[url_start + url_len] != ')')
             url_len++;
-          if (in[url_start + url_len] == ')')
-          {
+          if (in[url_start + url_len] == ')') {
             char link_html[LINE_BUFFER_SIZE];
             snprintf(link_html, sizeof(link_html), "<a href=\"%.*s\">%.*s</a>", (int) url_len,
                      in + url_start, (int) text_len, in + link_text_start);
             size_t link_html_len = strlen(link_html);
-            if (j + link_html_len < out_size - 1)
-            {
+            if (j + link_html_len < out_size - 1) {
               strcpy(&out[j], link_html);
               j += link_html_len;
             }
@@ -145,15 +127,13 @@ void inline_parse(const char* in, char* out, size_t out_size)
 // - List items (lines starting with "- " or "* ")
 // - Blockquotes (lines starting with '> ')
 // - Paragraphs (all other non-empty lines)
-void block_parse(const char* input, char* output, size_t out_size)
-{
+void block_parse(const char* input, char* output, size_t out_size) {
   char   line[LINE_BUFFER_SIZE];
   size_t output_index  = 0;
   int    in_code_block = 0;
 
   const char* p = input;
-  while (*p)
-  {
+  while (*p) {
     size_t line_len = 0;
     while (p[line_len] && p[line_len] != '\n' && line_len < LINE_BUFFER_SIZE - 1)
       line[line_len] = p[line_len], line_len++;
@@ -163,31 +143,25 @@ void block_parse(const char* input, char* output, size_t out_size)
     else
       p += line_len;
 
-    if (strncmp(line, "```", 3) == 0)
-    {
-      if (!in_code_block)
-      {
+    if (strncmp(line, "```", 3) == 0) {
+      if (!in_code_block) {
         strncat(output, "<pre><code>\n", out_size - strlen(output) - 1);
         in_code_block = 1;
-      }
-      else
-      {
+      } else {
         strncat(output, "\n</code></pre>\n", out_size - strlen(output) - 1);
         in_code_block = 0;
       }
       continue;
     }
 
-    if (in_code_block)
-    {
+    if (in_code_block) {
       // We're inside a code block, output the line verbatim
       strncat(output, line, out_size - strlen(output) - 1);
       strncat(output, "\n", out_size - strlen(output) - 1);
       continue;
     }
 
-    if (line[0] == '#')
-    {
+    if (line[0] == '#') {
       int header_level = 0;
       while (line[header_level] == '#' && header_level < 6)
         header_level++;
@@ -207,16 +181,14 @@ void block_parse(const char* input, char* output, size_t out_size)
     }
 
     // Blockquote
-    if (line[0] == '>')
-    {
+    if (line[0] == '>') {
       char blockquote_html[LINE_BUFFER_SIZE];
       snprintf(blockquote_html, sizeof(blockquote_html), "<blockquote>%s</blockquote>\n", line + 2);
       strncat(output, blockquote_html, out_size - strlen(output) - 1);
       continue;
     }
 
-    if ((line[0] == '-' || line[0] == '*') && line[1] == ' ')
-    {
+    if ((line[0] == '-' || line[0] == '*') && line[1] == ' ') {
       char inline_output[LINE_BUFFER_SIZE] = {0};
       inline_parse(line + 2, inline_output, sizeof(inline_output));
       char list_item[LINE_BUFFER_SIZE];
@@ -225,16 +197,13 @@ void block_parse(const char* input, char* output, size_t out_size)
       continue;
     }
 
-    if (strlen(line) > 0)
-    {
+    if (strlen(line) > 0) {
       char inline_output[LINE_BUFFER_SIZE] = {0};
       inline_parse(line, inline_output, sizeof(inline_output));
       char paragraph[LINE_BUFFER_SIZE];
       snprintf(paragraph, sizeof(paragraph), "<p>%s</p>\n", inline_output);
       strncat(output, paragraph, out_size - strlen(output) - 1);
-    }
-    else
-    {
+    } else {
       // Blank line.
       strncat(output, "\n", out_size - strlen(output) - 1);
     }
@@ -244,14 +213,12 @@ void block_parse(const char* input, char* output, size_t out_size)
 // Converts a markdown string into HTML
 // Caller must provide an output buffer (html) of at least html_size bytes
 // or suffer the consequences
-void md_to_html(const char* markdown, char* html, size_t html_size, const char* css_file)
-{
+void md_to_html(const char* markdown, char* html, size_t html_size, const char* css_file) {
   if (!html || html_size == 0)
     return;
 
   snprintf(html, html_size, "<!DOCTYPE html><html><head>");
-  if (css_file && strlen(css_file) > 0)
-  {
+  if (css_file && strlen(css_file) > 0) {
     snprintf(html + strlen(html), html_size - strlen(html), "<link rel=\"stylesheet\" href=\"%s\">",
              css_file);
   }
@@ -264,8 +231,7 @@ void md_to_html(const char* markdown, char* html, size_t html_size, const char* 
 
 // Convert a markdown file to an HTML file
 int md_file_to_html_file(const char* input_filename, const char* output_filename,
-                         const char* css_file)
-{
+                         const char* css_file) {
   FILE* fin = fopen(input_filename, "r");
   if (!fin)
     return -1;
@@ -275,8 +241,7 @@ int md_file_to_html_file(const char* input_filename, const char* output_filename
 
   fseek(fin, 0, SEEK_SET);
   char* markdown = malloc(filesize + 1);
-  if (!markdown)
-  {
+  if (!markdown) {
     fclose(fin);
     return -2;
   }
@@ -286,8 +251,7 @@ int md_file_to_html_file(const char* input_filename, const char* output_filename
   fclose(fin);
 
   char* html_output = malloc(OUTPUT_BUFFER_SIZE);
-  if (!html_output)
-  {
+  if (!html_output) {
     free(markdown);
     return -3;
   }
@@ -296,8 +260,7 @@ int md_file_to_html_file(const char* input_filename, const char* output_filename
   md_to_html(markdown, html_output, OUTPUT_BUFFER_SIZE, css_file);
 
   FILE* fout = fopen(output_filename, "w");
-  if (!fout)
-  {
+  if (!fout) {
     free(markdown);
     free(html_output);
     return -4;
@@ -313,12 +276,9 @@ int md_file_to_html_file(const char* input_filename, const char* output_filename
 
 // Process multiple markdown files
 int md_files_to_html_files(const char** input_files, const char** output_files, int count,
-                           const char* css_file)
-{
-  for (int i = 0; i < count; i++)
-  {
-    if (md_file_to_html_file(input_files[i], output_files[i], css_file) != 0)
-    {
+                           const char* css_file) {
+  for (int i = 0; i < count; i++) {
+    if (md_file_to_html_file(input_files[i], output_files[i], css_file) != 0) {
       return -1;
     }
   }
